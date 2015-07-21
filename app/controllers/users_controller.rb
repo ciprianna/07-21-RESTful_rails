8e3c8b3458c92d793d688dbd3efb32aff8bd9cf5
Ciprianna <ciprianna17@gmail.com>
2015-07-21 12:59:09 -0500

    Created authenticate login route and method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+
   def login
   end
+
+  def authenticate_login
+    entered_email = params["users"]["email"]
+    @user_email = User.find_by(email: entered_email)
+
+    if !@user_email.nil?
+      @valid = true
+      given_pw = params["users"]["password"]
+      actual_pw = BCrypt::Password.new(@user_email.password)
+      if actual_pw == given_pw
+        session[:user_id] = @user_email.id
+        render :"users/index"
+      else
+        @valid = false
+        render :"users/login"
+      end
+    else
+      @valid = false
+      render :"users/login"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   get "login" => "users#login"
+
+  post "authenticate_login" => "users#authenticate_login"
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
